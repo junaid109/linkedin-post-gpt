4e2194b818ffb1c529884977088685972c9b4710
Junaid Malik <junmalik109@gmail.com>
2023-03-09 15:43:10 +0000

    Create generate.ts

---
+++ pages/api/generate.ts
@@ -0,0 +1,42 @@
+import { OpenAIStream, OpenAIStreamPayload } from "../../utils/OpenAIStream";
+
+if (!process.env.OPENAI_API_KEY) {
+  throw new Error("Missing env var from OpenAI");
+}
+
+export const config = {
+  runtime: "edge",
+};
+
+const handler = async (req: Request): Promise<Response> => {
+  let { prompt } = (await req.json()) as {
+    prompt?: string;
+  };
+
+  if (!prompt) {
+    return new Response("No prompt in the request", { status: 400 });
+  }
+
+  const payload: OpenAIStreamPayload = {
+    model: "gpt-3.5-turbo",
+    messages: [{ role: "user", content: prompt }],
+    temperature: 0.7,
+    top_p: 1,
+    frequency_penalty: 0,
+    presence_penalty: 0,
+    max_tokens: 200,
+    stream: true,
+    n: 1,
+  };
+
+  let stream: ReadableStream;
+  try {
+    stream = await OpenAIStream(payload);
+  } catch (err) {
+    return new Response(err.message, { status: 500 });
+  }
+
+  return new Response(stream);
+};
+
+export default handler;
